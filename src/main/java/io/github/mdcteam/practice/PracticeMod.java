49ed3ef794d94dcd3d93ca8f75eedc3d5b443b85
bright_spark <markpassey@btinternet.com>
2018-01-16 00:09:37 +0000

    Removed the unnecessary Reference class Using the gradle.properties for properties in the build.gradle build.gradle will now add the version into the variable in the main mod class

--- src/main/java/io/github/mdcteam/practice/PracticeMod.java
+++ src/main/java/io/github/mdcteam/practice/PracticeMod.java
@@ -6,8 +6,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
-public class Main {
+@Mod(modid = PracticeMod.MOD_ID, name = PracticeMod.NAME, version = PracticeMod.VERSION)
+public class PracticeMod
+{
+    public static final String MOD_ID = "practice";
+    public static final String NAME = "Practice Mod";
+    public static final String VERSION = "@VERSION@"; //Don't change this - it's being replaced by the build.gradle
 
     @EventHandler
     public static void preInit(FMLPreInitializationEvent e) {
